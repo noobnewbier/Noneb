64d04947c758c6221543bcffaa725c1b4131e4af
noobnewbier7 <peter789b@gmail.com>
2020-07-23 23:24:23 +0800

    Common constants

---
+++ Assets/Scripts/Common/Constants/DirectoryNames.cs
@@ -0,0 +1,11 @@
+﻿namespace Common.Constants
+{
+    public static class DirectoryNames
+    {
+#if UNITY_EDITOR
+        public static string Environments => "Environments";
+#else
+        public static string Environments => Application.persistentDataPath + "//Environments";
+#endif
+    }
+}

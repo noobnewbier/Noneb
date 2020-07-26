ccdc36e68c4f58c7b7fad13998230f8042e2226c
noobnewbier7 <peter789b@gmail.com>
2020-07-26 15:26:53 +0800

    Update LiveData to use versioning instead of instance checking

--- Assets/Scripts/Experiment/CrossPlatformLiveData/LiveData.cs
+++ Assets/Scripts/Experiment/CrossPlatformLiveData/LiveData.cs
@@ -1,6 +1,20 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
 using Experiment.CrossPlatformLiveData.Internal.Facade;
 using UniRx;
+
+/*
+ * The original implementer did a weird job translating this,
+ * probably because he is not really capturing the whole thing but instead mimicking it.
+ * Issue so far:
+ *     1. doesn't make sense to use subscribe, should implement "Observe" instead
+ *     2. Not sure about thread safety...
+ *     3. ATTEMPTED TO FIX: try to use versioning instead of instance matching(which makes more sense anyway)
+ *
+ * consider to fully(or partly) translate the whole thing from Android instead of using this:
+ * https://github.com/aosp-mirror/platform_frameworks_support/blob/b9cd83371e928380610719dfbf97c87c58e80916/lifecycle/lifecycle-livedata-core/src/main/java/androidx/lifecycle/LiveData.java
+ */
 
 namespace Experiment.CrossPlatformLiveData
 {
@@ -8,34 +22,32 @@ namespace Experiment.CrossPlatformLiveData
     {
         private readonly BehaviorSubject<T> _subject;
         private readonly IRxSchedulersFacade _rxSchedulers = new RxSchedulersFacade();
-        private readonly bool _allowDuplicatesInSequenceAndOnResumeFlag;
-        private T _lastEmitted;
+        private readonly ConcurrentBag<LiveDataObserverWrapper> _observers;
+
+        #region Added by Noob
+
+        private int _version;
+        private const int StartVersion = -1;
+
+        #endregion
 
         /// <summary>
         /// First emitted value will be type default
         /// </summary>
-        /// <param name="allowDuplicatesInSequenceAndOnResume">
-        /// If set emitting the same value one after another is allowed,
-        /// Will also cause value re-emission onResume lifecycle event
-        /// </param>
-        public LiveData(bool allowDuplicatesInSequenceAndOnResume = false)
+        public LiveData()
         {
             _subject = new BehaviorSubject<T>(default);
-            _allowDuplicatesInSequenceAndOnResumeFlag = allowDuplicatesInSequenceAndOnResume;
+            _version = StartVersion;
         }
 
         /// <summary>
         /// Emit first value with provided default
         /// </summary>
         /// <param name="initValue">Initial emitted value</param>
-        /// <param name="allowDuplicatesInSequenceAndOnResume">
-        /// If set emitting the same value one after another is allowed,
-        /// Will also cause value re-emission onResume lifecycle event
-        /// </param>
-        public LiveData(T initValue, bool allowDuplicatesInSequenceAndOnResume = false)
+        public LiveData(T initValue)
         {
             _subject = new BehaviorSubject<T>(initValue);
-            _allowDuplicatesInSequenceAndOnResumeFlag = allowDuplicatesInSequenceAndOnResume;
+            _version = StartVersion + 1;
         }
 
         /// <summary>
@@ -43,17 +55,11 @@ namespace Experiment.CrossPlatformLiveData
         /// </summary>
         /// <param name="initValue">Initial emitted value</param>
         /// <param name="rxSchedulers">Use custom IRxSchedulersFacade implementation</param>
-        /// <param name="allowDuplicatesInSequenceAndOnResume">
-        /// If set emitting the same value one after another is allowed,
-        /// by default duplicates can be emitted only if there was another value in-between them in a sequence.
-        /// Will also cause value re-emission onResume lifecycle event
-        /// </param>
-        public LiveData(T initValue, IRxSchedulersFacade rxSchedulers, bool allowDuplicatesInSequenceAndOnResume = false)
+        public LiveData(T initValue, IRxSchedulersFacade rxSchedulers)
         {
- 
             _subject = new BehaviorSubject<T>(initValue);
             _rxSchedulers = rxSchedulers;
-            _allowDuplicatesInSequenceAndOnResumeFlag = allowDuplicatesInSequenceAndOnResume;
+            _version = StartVersion + 1;
         }
 
         public T Value { get; private set; }
@@ -64,6 +70,7 @@ namespace Experiment.CrossPlatformLiveData
         /// <param name="value"></param>
         public void PostValue(T value)
         {
+            Interlocked.Increment(ref _version);
             _subject.OnNext(value);
         }
 
@@ -77,30 +84,11 @@ namespace Experiment.CrossPlatformLiveData
         /// <returns>IDisposable</returns>
         public IDisposable Subscribe(Action<T> onNext, Action<Exception> onError, Action onCompleted)
         {
-            return _subject.SubscribeOn(_rxSchedulers.Io())
+            var observerWrapper = new LiveDataObserverWrapper(Observer.Create(onNext, onError, onCompleted));
+            return _subject.Select(d => new VersionedData(d, _version))
+                .SubscribeOn(_rxSchedulers.Io())
                 .ObserveOn(_rxSchedulers.Ui())
-                .Subscribe(
-                    obj =>
-                    {
-                        if (obj != null && _allowDuplicatesInSequenceAndOnResumeFlag)
-                        {
-                            Value = obj;
-                            onNext.Invoke(obj);
-                        }
-                        else
-                        {
-                            if (obj != null && !obj.Equals(_lastEmitted))
-                            {
-                                Value = obj;
-                                onNext.Invoke(obj);
-                            }
-
-                            _lastEmitted = obj;
-                        }
-                    },
-                    onError,
-                    onCompleted
-                );
+                .Subscribe(observerWrapper);
         }
 
         #region Added by Noob
@@ -114,29 +102,11 @@ namespace Experiment.CrossPlatformLiveData
         /// <returns>IDisposable</returns>
         public IDisposable Subscribe(Action<T> onNext, Action<Exception> onError)
         {
-            return _subject.SubscribeOn(_rxSchedulers.Io())
+            var observerWrapper = new LiveDataObserverWrapper(Observer.Create(onNext, onError));
+            return _subject.Select(d => new VersionedData(d, _version))
+                .SubscribeOn(_rxSchedulers.Io())
                 .ObserveOn(_rxSchedulers.Ui())
-                .Subscribe(
-                    obj =>
-                    {
-                        if (obj != null && _allowDuplicatesInSequenceAndOnResumeFlag)
-                        {
-                            Value = obj;
-                            onNext.Invoke(obj);
-                        }
-                        else
-                        {
-                            if (obj != null && !obj.Equals(_lastEmitted))
-                            {
-                                Value = obj;
-                                onNext.Invoke(obj);
-                            }
-
-                            _lastEmitted = obj;
-                        }
-                    },
-                    onError
-                );
+                .Subscribe(observerWrapper);
         }
 
         /// <summary>
@@ -147,28 +117,57 @@ namespace Experiment.CrossPlatformLiveData
         /// <returns>IDisposable</returns>
         public IDisposable Subscribe(Action<T> onNext)
         {
-            return _subject.SubscribeOn(_rxSchedulers.Io())
+            var observerWrapper = new LiveDataObserverWrapper(Observer.Create(onNext));
+            return _subject.Select(d => new VersionedData(d, _version))
+                .SubscribeOn(_rxSchedulers.Io())
                 .ObserveOn(_rxSchedulers.Ui())
-                .Subscribe(
-                    obj =>
-                    {
-                        if (obj != null && _allowDuplicatesInSequenceAndOnResumeFlag)
-                        {
-                            Value = obj;
-                            onNext.Invoke(obj);
-                        }
-                        else
-                        {
-                            if (obj != null && !obj.Equals(_lastEmitted))
-                            {
-                                Value = obj;
-                                onNext.Invoke(obj);
-                            }
+                .Subscribe(observerWrapper);
+        }
 
-                            _lastEmitted = obj;
-                        }
-                    }
-                );
+        private class VersionedData
+        {
+            public VersionedData(T data, int version)
+            {
+                Data = data;
+                Version = version;
+            }
+
+            public T Data { get; }
+            public int Version { get; }
+        }
+
+        private class LiveDataObserverWrapper : IObserver<VersionedData>
+        {
+            private int LastVersion { get; set; } = StartVersion;
+            private readonly IObserver<T> _observer;
+
+            public LiveDataObserverWrapper(IObserver<T> observer)
+            {
+                _observer = observer;
+            }
+
+
+            public void OnCompleted()
+            {
+                _observer.OnCompleted();
+            }
+
+            public void OnError(Exception error)
+            {
+                _observer.OnError(error);
+            }
+
+            public void OnNext(VersionedData value)
+            {
+                if (LastVersion >= value.Version)
+                {
+                    return;
+                }
+
+                LastVersion = value.Version;
+
+                _observer.OnNext(value.Data);
+            }
         }
 
         #endregion

8436bd22f4dbc695867aa84317bda481cfae404d
noobnewbier7 <peter789b@gmail.com>
2020-08-04 18:37:37 +0800

    Added mechanism to fix stronghold's positioning(unit should stand over a turret for example) after loader all holders

---
+++ Assets/Scripts/GameEnvironments/Load/BoardItemOnTile/StrongholdInternalPosition/StrongholdGameObjectsInternalPositionSetupper.cs
@@ -0,0 +1,52 @@
+﻿using Common.Constants;
+using GameEnvironments.Common.Repositories.CurrentGameEnvironment;
+using Tiles;
+using UnityEngine;
+using UnityUtils;
+
+namespace GameEnvironments.Load.BoardItemOnTile.StrongholdInternalPosition
+{
+    public class StrongholdGameObjectsInternalPositionSetupper : MonoBehaviour
+    {
+        [SerializeField] private SetupStrongholdGameObjectsInternalPositionServiceProvider serviceProvider;
+        [SerializeField] private TilesTransformProvider tilesTransformProvider;
+        [SerializeField] private CurrentGameEnvironmentRepositoryProvider currentGameEnvironmentRepositoryProvider;
+
+        private ISetupStrongholdGameObjectsInternalPositionService _setupStrongholdGameObjectsInternalPositionService;
+        private ICurrentGameEnvironmentRepository _currentGameEnvironmentRepository;
+
+        private void OnEnable()
+        {
+            _setupStrongholdGameObjectsInternalPositionService = serviceProvider.Provide();
+            _currentGameEnvironmentRepository = currentGameEnvironmentRepositoryProvider.Provide();
+        }
+
+        [ContextMenu(nameof(Setup))]
+        public void Setup()
+        {
+            var tiles = tilesTransformProvider.Provide();
+            var strongholdDatas = _currentGameEnvironmentRepository.Get().StrongholdDatas;
+
+            for (var i = 0; i < strongholdDatas.Length; i++)
+            {
+                if (strongholdDatas[i] == null)
+                {
+                    continue;
+                }
+
+                SetupTileWithStronghold(tiles[i]);
+            }
+        }
+
+        private void SetupTileWithStronghold(Transform tileTransform)
+        {
+            var unitGameObject = tileTransform.FindChildWithTag(ObjectTags.UnitGameObject).gameObject;
+            var constructGameObject = tileTransform.FindChildWithTag(ObjectTags.ConstructGameObject).gameObject;
+
+            _setupStrongholdGameObjectsInternalPositionService.SetPosition(
+                unitGameObject,
+                constructGameObject
+            );
+        }
+    }
+}

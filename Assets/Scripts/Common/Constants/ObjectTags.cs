0eafa0c60f2b4db69815b252580ab9647b09f56a
noobnewbier7 <peter789b@gmail.com>
2020-08-04 18:17:50 +0800

    Added tags for Unit and Construct GameObject

--- Assets/Scripts/Common/Constants/ObjectTags.cs
+++ Assets/Scripts/Common/Constants/ObjectTags.cs
@@ -2,10 +2,22 @@
 {
     public static class ObjectTags
     {
+        #region Hodlers
+
         public const string GridRow = "GridRow";
         public const string TileHolder = "TileHolder";
         public const string UnitHolder = "UnitHolder";
         public const string ConstructHolder = "ConstructHolder";
         public const string StrongholdHolder = "StrongholdHolder";
+
+        #endregion
+
+
+        #region BoardItem's GameObjects
+
+        public const string UnitGameObject = "UnitGameObject";
+        public const string ConstructGameObject = "ConstructGameObject";
+
+        #endregion
     }
 }

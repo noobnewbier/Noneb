37eb48296bb9bd513b0153631f0ae1d3a1c1e0dd
noobnewbier7 <peter789b@gmail.com>
2020-09-20 16:12:06 +0800

    Utilize IPoolable

--- Assets/Scripts/Common/Holders/IBoardItemHolder.cs
+++ Assets/Scripts/Common/Holders/IBoardItemHolder.cs
@@ -1,10 +1,12 @@
 ﻿using Common.BoardItems;
 using UnityEngine;
+using UnityUtils;
+using UnityUtils.Pooling;
 
 namespace Common.Holders
 {
     //Do we ACTUALLY need this, in theory we can probably get away without it considering all the holders are doing is showing gizmos atm
-    public interface IBoardItemHolder
+    public interface IBoardItemHolder : IPoolable<GameObject>
     {
         Transform Transform { get; }
     }
--- Assets/Scripts/Common/Providers/PooledMonoBehaviourProvider.cs
+++ Assets/Scripts/Common/Providers/PooledMonoBehaviourProvider.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using UnityEngine;
 using UnityUtils;
+using UnityUtils.Pooling;
 
 namespace Common.Providers
 {

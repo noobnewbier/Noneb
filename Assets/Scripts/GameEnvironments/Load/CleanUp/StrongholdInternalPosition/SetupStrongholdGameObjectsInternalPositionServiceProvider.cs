b9990e2a9e1ef94bac21048a2558542d6ab25bb5
noobnewbier7 <peter789b@gmail.com>
2020-09-20 16:43:05 +0800

    Fixing StrongholdInternalPosition related

---
+++ Assets/Scripts/GameEnvironments/Load/CleanUp/StrongholdInternalPosition/SetupStrongholdGameObjectsInternalPositionServiceProvider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Common.Providers;
+using GameEnvironments.Load.CleanUp.StrongholdInternalPosition;
+using UnityEngine;
+using UnityUtils.Constants;
+
+namespace GameEnvironments.Load.Obsolete.BoardItemOnTile.StrongholdInternalPosition
+{
+    [CreateAssetMenu(
+        fileName = nameof(SetupStrongholdGameObjectsInternalPositionServiceProvider),
+        menuName = MenuName.ScriptableService + nameof(SetupStrongholdGameObjectsInternalPositionService)
+    )]
+    public class SetupStrongholdGameObjectsInternalPositionServiceProvider : ScriptableObjectProvider<
+        ISetupStrongholdGameObjectsInternalPositionService>
+    {
+        private readonly Lazy<ISetupStrongholdGameObjectsInternalPositionService> _lazyInstance =
+            new Lazy<ISetupStrongholdGameObjectsInternalPositionService>(() => new SetupStrongholdGameObjectsInternalPositionService());
+
+        public override ISetupStrongholdGameObjectsInternalPositionService Provide()
+        {
+            return _lazyInstance.Value;
+        }
+    }
+}
---
+++ Assets/Scripts/GameEnvironments/Load/CleanUp/StrongholdInternalPosition/StrongholdGameObjectsInternalPositionLoader.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using Common;
+using Common.Constants;
+using Common.Holders;
+using Common.Loaders;
+using GameEnvironments.Common.Repositories.BoardItemsHolder;
+using GameEnvironments.Common.Repositories.BoardItemsHolder.Providers;
+using GameEnvironments.Load.Obsolete.BoardItemOnTile.StrongholdInternalPosition;
+using Strongholds;
+using UniRx;
+using UnityEngine;
+using UnityUtils;
+
+namespace GameEnvironments.Load.CleanUp.StrongholdInternalPosition
+{
+    [CreateAssetMenu(
+        fileName = nameof(StrongholdGameObjectsInternalPositionLoader),
+        menuName = ProjectMenuName.Loader + nameof(StrongholdGameObjectsInternalPositionLoader)
+    )]
+    public class StrongholdGameObjectsInternalPositionLoader : ScriptableObject, ILoader
+    {
+        [SerializeField] private SetupStrongholdGameObjectsInternalPositionServiceProvider serviceProvider;
+        [SerializeField] private StrongholdsHolderRepositoryProvider strongholdsHolderRepositoryProvider;
+
+        private ISetupStrongholdGameObjectsInternalPositionService _setupStrongholdGameObjectsInternalPositionService;
+        private IBoardItemsHolderRepository<StrongholdHolder> _strongholdsHolderRepository;
+        private IDisposable _disposable;
+
+        [ContextMenu(nameof(LoadAndForget))]
+        public void LoadAndForget()
+        {
+            DisposeDisposables();
+
+            _disposable = _strongholdsHolderRepository.GetMostRecent()
+                .Subscribe(SetupTilesWithStronghold);
+        }
+
+        public IObservable<Unit> LoadObservable()
+        {
+            return _strongholdsHolderRepository.GetMostRecent()
+                .Select(
+                    holders =>
+                    {
+                        SetupTilesWithStronghold(holders);
+
+                        return Unit.Default;
+                    }
+                );
+        }
+
+        private void OnEnable()
+        {
+            _setupStrongholdGameObjectsInternalPositionService = serviceProvider.Provide();
+            _strongholdsHolderRepository = strongholdsHolderRepositoryProvider.Provide();
+        }
+
+        private void SetupTilesWithStronghold(IReadOnlyList<StrongholdHolder> strongholdHolders)
+        {
+            foreach (var strongholdHolder in strongholdHolders)
+            {
+                if (strongholdHolder == null)
+                {
+                    continue;
+                }
+
+                SetupIndividualTileWithStronghold(strongholdHolder);
+            }
+        }
+
+        private void SetupIndividualTileWithStronghold(IBoardItemHolder strongholdHolder)
+        {
+            var strongholdTransform = strongholdHolder.Transform;
+            var unitGameObject = strongholdTransform.FindChildWithTag(ObjectTags.UnitGameObject).gameObject;
+            var constructGameObject = strongholdTransform.FindChildWithTag(ObjectTags.ConstructGameObject).gameObject;
+
+            _setupStrongholdGameObjectsInternalPositionService.SetPosition(
+                unitGameObject,
+                constructGameObject
+            );
+        }
+
+        private void OnDisable()
+        {
+            DisposeDisposables();
+        }
+
+        private void DisposeDisposables()
+        {
+            _disposable?.Dispose();
+        }
+    }
+}

43f371ad7ee6ef0e086f6ea6828a1c17f2c322b6
noobnewbier7 <peter789b@gmail.com>
2020-09-29 17:26:49 +0800

    cleaned up no longer true comments

--- Assets/Scripts/Strongholds/StrongholdData.cs
+++ Assets/Scripts/Strongholds/StrongholdData.cs
@@ -5,10 +5,6 @@ using UnityEngine;
 
 namespace Strongholds
 {
-    /// <summary>
-    /// Unlike other data class, this is not a scriptable as it should not be created manually,
-    /// it should be created at runtime through player's interaction between unit and construct
-    /// </summary>
     public class StrongholdData : BoardItemData
     {
         private StrongholdData(Sprite icon, string name, ConstructData constructData, UnitData unitData) :

0c62f45ff362f5eb7d47973b69fb8219bdf3e838
noobnewbier7 <peter789b@gmail.com>
2020-10-08 15:53:01 +0800

    clean up

--- Assets/Code/Main/Ui/InGameEditor/SelectedEditorPalettes/SelectedEditorPaletteRepositoryProvider.cs
+++ Assets/Code/Main/Ui/InGameEditor/SelectedEditorPalettes/SelectedEditorPaletteRepositoryProvider.cs
@@ -1,10 +1,11 @@
 ﻿using Main.Core.Game.Common.Providers;
 using Main.Core.InGameEditor.Data;
 using UnityEngine;
+using UnityUtils.Constants;
 
 namespace Main.Ui.InGameEditor.SelectedEditorPalettes
 {
-    [CreateAssetMenu(fileName = nameof(SelectedEditorPaletteRepositoryProvider), menuName = "ScriptableRepository/SelectedEditorPaletteRepository")]
+    [CreateAssetMenu(fileName = nameof(SelectedEditorPaletteRepositoryProvider), menuName = MenuName.ScriptableRepository + nameof(SelectedEditorPaletteRepository))]
     public class SelectedEditorPaletteRepositoryProvider : ScriptableObject, IObjectProvider<ISelectedEditorPaletteRepository>
     {
         [SerializeField] private EditorPalette editorPalette;

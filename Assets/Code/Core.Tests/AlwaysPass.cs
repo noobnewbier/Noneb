98175c321322d36962922be4697d3c2643d8fa9f
noobnewbier7 <peter789b@gmail.com>
2020-10-10 16:09:32 +0800

    Added tests

---
+++ Assets/Code/Core.Tests/AlwaysPass.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace Core.Tests
+{
+    public class AlwaysPass
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void AlwaysPassSimplePasses()
+        {
+            // Use the Assert class to test conditions
+        }
+
+    }
+}
---
+++ Assets/Code/Core.Tests/Game/Coordinates/CoordinateServiceTests.cs
@@ -0,0 +1,74 @@
+﻿using Main.Core.Game.Coordinates;
+using Main.Core.Game.Maps;
+using NUnit.Framework;
+
+namespace Core.Tests.Game.Coordinates
+{
+    [TestFixture]
+    public class CoordinateServiceTests
+    {
+        public class GetAxialCoordinateFromNestedArrayIndex
+        {
+            private CoordinateService _coordinateService;
+
+            [SetUp]
+            public void SetUp()
+            {
+                _coordinateService = new CoordinateService();
+            }
+
+            [Test]
+            public void Given_X2_Z2_ReturnCorrectAnswer_X3_Z2()
+            {
+                var expectedAnswer = new Coordinate(3, 2);
+
+                var returnedValue = _coordinateService.GetAxialCoordinateFromNestedArrayIndex(2, 2);
+
+                Assert.AreEqual(expectedAnswer, returnedValue);
+            }
+
+            [Test]
+            public void Given_X0_Z0_ReturnCorrectAnswer_X0_Z0()
+            {
+                var expectedAnswer = new Coordinate(0, 0);
+
+                var returnedValue = _coordinateService.GetAxialCoordinateFromNestedArrayIndex(0, 0);
+
+                Assert.AreEqual(expectedAnswer, returnedValue);
+            }
+        }
+
+        public class GetCoordinateFromFlattenArrayIndex
+        {
+            private CoordinateService _coordinateService;
+            private MapConfig _threeByThreeConfig;
+
+            [SetUp]
+            public void SetUp()
+            {
+                _coordinateService = new CoordinateService();
+                _threeByThreeConfig = MapConfig.Create(3, 3);
+            }
+
+            [Test]
+            public void Given_Param1As8_Param2As3x3_ReturnCorrectAnswer_X3_Z2()
+            {
+                var expectedAnswer = new Coordinate(3, 2);
+
+                var returnedValue = _coordinateService.GetCoordinateFromFlattenArrayIndex(8, _threeByThreeConfig);
+
+                Assert.AreEqual(expectedAnswer, returnedValue);
+            }
+
+            [Test]
+            public void Given_Param1As0_Param2As3x3_ReturnCorrectAnswer_X0_Z0()
+            {
+                var expectedAnswer = new Coordinate(0, 0);
+
+                var returnedValue = _coordinateService.GetCoordinateFromFlattenArrayIndex(0, _threeByThreeConfig);
+
+                Assert.AreEqual(expectedAnswer, returnedValue);
+            }
+        }
+    }
+}

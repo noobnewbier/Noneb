a49f90b918047b2533ae5fb4fe5b3c8a5352780d
noobnewbier7 <peter789b@gmail.com>
2020-10-12 17:55:43 +0800

    added test for LevelDataScriptableValidationService

---
+++ Assets/Code/Tests/Core.Game.Tests/GameEnvironments/Save/LevelDataScriptableValidationServiceTest.cs
@@ -0,0 +1,130 @@
+﻿using Main.Core.Game.GameEnvironments.Data.LevelDatas;
+using Main.Core.Game.GameEnvironments.Validation;
+using Main.Core.Game.Maps;
+using Main.Core.Game.Units;
+using NUnit.Framework;
+using UnityEngine;
+using static Core.Game.Tests.GameEnvironments.Save.LevelDataTestExtensions;
+
+namespace Core.Game.Tests.GameEnvironments.Save
+{
+    [TestFixture]
+    public class LevelDataScriptableValidationServiceTest
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            _service = new LevelDataScriptableValidationService();
+            _3X3MapConfig = MapConfig.Create(3, 3);
+        }
+
+        private LevelDataScriptableValidationService _service;
+        private MapConfig _3X3MapConfig;
+
+        [Test]
+        public void WhenAllFieldsAreValid_ReturnIsValid()
+        {
+            var validLevelData = GetEmptyLevelData(_3X3MapConfig).FillWithTiles().ToScriptable();
+
+            var returnedValue = _service.Validate(validLevelData, _3X3MapConfig);
+
+            Assert.That(returnedValue.IsValid, Is.True);
+        }
+
+        [Test]
+        public void WhenNotAllDataOfCorrectSize_ReturnIsNotValid()
+        {
+            var emptyLevelData = LevelDataScriptable.Create(LevelData.Empty);
+
+            var returnedValue = _service.Validate(emptyLevelData, _3X3MapConfig);
+
+            Assert.That(returnedValue.IsValid, Is.False);
+        }
+
+        [Test]
+        public void WhenNotAllTileHasData_ReturnIsNotValid()
+        {
+            var levelData = GetEmptyLevelData(_3X3MapConfig).FillWithTiles().ToScriptable();
+            levelData.TileDatas[0] = null;
+
+            var returnedValue = _service.Validate(levelData, _3X3MapConfig);
+
+            Assert.That(returnedValue.IsValid, Is.False);
+        }
+
+        [Test]
+        public void WhenStrongholdDataWrapperIsSetUpWithUnitButNotConstruct_ReturnIsNotValid()
+        {
+            var levelData = GetEmptyLevelData(_3X3MapConfig).FillWithTiles().FillWithStrongholds().ToScriptable();
+            levelData.StrongholdDatas[0] = new LevelDataScriptable.StrongholdDataWrapper(ScriptableObject.CreateInstance<UnitDataScriptable>(), null);
+
+            var returnedValue = _service.Validate(levelData, _3X3MapConfig);
+
+            Assert.That(returnedValue.IsValid, Is.False);
+        }
+
+        [Test]
+        public void WhenStrongholdHasDataButNotConstructGameObjectFactory_ReturnIsNotValid()
+        {
+            var levelData = GetEmptyLevelData(_3X3MapConfig).FillWithTiles().FillWithStrongholds().ToScriptable();
+            levelData.StrongholdConstructGameObjectProviders[0] = null;
+
+            var returnedValue = _service.Validate(levelData, _3X3MapConfig);
+
+            Assert.That(returnedValue.IsValid, Is.False);
+        }
+
+        [Test]
+        public void WhenStrongholdHasDataButNotUnitGameObjectFactory_ReturnIsNotValid()
+        {
+            var levelData = GetEmptyLevelData(_3X3MapConfig).FillWithTiles().FillWithStrongholds().ToScriptable();
+            levelData.StrongholdUnitGameObjectProviders[0] = null;
+
+            var returnedValue = _service.Validate(levelData, _3X3MapConfig);
+
+            Assert.That(returnedValue.IsValid, Is.False);
+        }
+
+        [Test]
+        public void WhenThereIsBothStrongholdAndConstructInSameCoordinate_ReturnIsNotValid()
+        {
+            var levelData = GetEmptyLevelData(_3X3MapConfig).FillWithTiles().FillWithConstructs().FillWithStrongholds().ToScriptable();
+
+            var returnedValue = _service.Validate(levelData, _3X3MapConfig);
+
+            Assert.That(returnedValue.IsValid, Is.False);
+        }
+
+        [Test]
+        public void WhenThereIsBothStrongholdAndUnitInSameCoordinate_ReturnIsNotValid()
+        {
+            var levelData = GetEmptyLevelData(_3X3MapConfig).FillWithTiles().FillWithUnits().FillWithStrongholds().ToScriptable();
+
+            var returnedValue = _service.Validate(levelData, _3X3MapConfig);
+
+            Assert.That(returnedValue.IsValid, Is.False);
+        }
+
+        [Test]
+        public void WhenUnitHasDataButNotGameObjectFactory_ReturnIsNotValid()
+        {
+            var levelData = GetEmptyLevelData(_3X3MapConfig).FillWithTiles().FillWithUnits().ToScriptable();
+            levelData.UnitGameObjectProviders[0] = null;
+
+            var returnedValue = _service.Validate(levelData, _3X3MapConfig);
+
+            Assert.That(returnedValue.IsValid, Is.False);
+        }
+
+        [Test]
+        public void WhenUnitHasGameObjectFactoryButNotData_ReturnIsNotValid()
+        {
+            var levelData = GetEmptyLevelData(_3X3MapConfig).FillWithTiles().FillWithUnits().ToScriptable();
+            levelData.UnitDatas[0] = null;
+
+            var returnedValue = _service.Validate(levelData, _3X3MapConfig);
+
+            Assert.That(returnedValue.IsValid, Is.False);
+        }
+    }
+}
---
+++ Assets/Code/Tests/Core.Game.Tests/GameEnvironments/Save/LevelDataTestExtensions.cs
@@ -0,0 +1,106 @@
+﻿using System.Linq;
+using Main.Core.Game.Common.Factories;
+using Main.Core.Game.Constructs;
+using Main.Core.Game.GameEnvironments.Data.LevelDatas;
+using Main.Core.Game.Maps;
+using Main.Core.Game.Strongholds;
+using Main.Core.Game.Tiles;
+using Main.Core.Game.Units;
+using UnityEngine;
+using UnityUtils;
+
+namespace Core.Game.Tests.GameEnvironments.Save
+{
+    public static class LevelDataTestExtensions
+    {
+        public static LevelData GetEmptyLevelData(MapConfig config)
+        {
+            var mapSize = config.GetTotalMapSize();
+            var tileDatas = Enumerable.Repeat<TileData>(
+                    null,
+                    mapSize
+                )
+                .ToArray();
+            var constructDatas = Enumerable.Repeat<ConstructData>(
+                    null,
+                    mapSize
+                )
+                .ToArray();
+
+            var unitDatas = Enumerable.Repeat<UnitData>(
+                    null,
+                    mapSize
+                )
+                .ToArray();
+
+            var strongholdDatas = Enumerable.Repeat<StrongholdData>(
+                    null,
+                    mapSize
+                )
+                .ToArray();
+
+            GameObjectFactory[] GetEmptyGoFactories()
+            {
+                return Enumerable.Repeat<GameObjectFactory>(
+                        null,
+                        mapSize
+                    )
+                    .ToArray();
+            }
+
+
+            return new LevelData(
+                tileDatas,
+                GetEmptyGoFactories(),
+                constructDatas,
+                GetEmptyGoFactories(),
+                unitDatas,
+                GetEmptyGoFactories(),
+                strongholdDatas,
+                new GameObjectFactory[mapSize],
+                new GameObjectFactory[mapSize]
+            );
+        }
+
+        public static LevelDataScriptable ToScriptable(this LevelData data) => LevelDataScriptable.Create(data);
+
+        public static LevelData FillWithTiles(this LevelData data)
+        {
+            data.TileDatas.Fill(new TileData(null, string.Empty, ScriptableObject.CreateInstance<TileDataScriptable>()));
+            data.TileGameObjectProviders.Fill(ScriptableObject.CreateInstance<GameObjectFactory>());
+
+            return data;
+        }
+
+        public static LevelData FillWithUnits(this LevelData data)
+        {
+            data.UnitDatas.Fill(new UnitData(null, string.Empty, ScriptableObject.CreateInstance<UnitDataScriptable>()));
+            data.UnitGameObjectProviders.Fill(ScriptableObject.CreateInstance<GameObjectFactory>());
+
+            return data;
+        }
+
+        public static LevelData FillWithConstructs(this LevelData data)
+        {
+            data.ConstructDatas.Fill(new ConstructData(null, string.Empty, ScriptableObject.CreateInstance<ConstructDataScriptable>()));
+            data.ConstructGameObjectProviders.Fill(ScriptableObject.CreateInstance<GameObjectFactory>());
+
+            return data;
+        }
+
+        public static LevelData FillWithStrongholds(this LevelData data)
+        {
+            data.StrongholdDatas.Fill(
+                StrongholdData.Create(
+                    new ConstructData(null, string.Empty, ScriptableObject.CreateInstance<ConstructDataScriptable>()),
+                    new UnitData(null, string.Empty, ScriptableObject.CreateInstance<UnitDataScriptable>())
+                )
+            );
+
+            data.StrongholdConstructGameObjectProviders.Fill(ScriptableObject.CreateInstance<GameObjectFactory>());
+            data.StrongholdUnitGameObjectProviders.Fill(ScriptableObject.CreateInstance<GameObjectFactory>());
+
+            return data;
+        }
+    }
+}

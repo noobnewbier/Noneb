5645858b740e31c46760b47b580f834e426d8185
noobnewbier7 <peter789b@gmail.com>
2020-10-15 22:41:44 +0800

    clean up

--- Assets/Code/Main/Ui/Game/UiState/BoardItemsFetcher/BoardItemsHolderFetcherRepository.cs
+++ Assets/Code/Main/Ui/Game/UiState/BoardItemsFetcher/BoardItemsHolderFetcherRepository.cs
@@ -1,7 +1,6 @@
 ﻿using Main.Core.Game.Common;
 using Main.Ui.Game.Common.Holders;
 
-//todo: your namespace fucked up
 namespace Main.Ui.Game.UiState.BoardItemsFetcher
 {
     public class BoardItemsHolderFetcherRepository<THolderProvider, THolder> : DataRepository<THolderProvider>

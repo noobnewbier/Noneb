5967f9bd28a4c130ec7ebd71db126373500bbf39
noobnewbier7 <peter789b@gmail.com>
2020-10-16 17:23:46 +0800

    sharing current inspectable state through a repo

---
+++ Assets/Code/Noneb/Ui/InGameEditor/UiState/Inspectable/CurrentInspectableRepositoryProvider.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Noneb.Core.Game.Common;
+using Noneb.Core.Game.Common.Constants;
+using Noneb.Core.Game.Common.Providers;
+using Noneb.Core.InGameEditor.Common;
+using UnityUtils.Constants;
+
+namespace Noneb.Ui.InGameEditor.UiState.Inspectable
+{
+    [UnityEngine.CreateAssetMenu(fileName = nameof(CurrentInspectableRepositoryProvider), menuName = MenuName.ScriptableRepository + ProjectMenuName.InGameEditor + nameof(IInspectable))]
+    public class CurrentInspectableRepositoryProvider : UnityEngine.ScriptableObject, IObjectProvider<IDataRepository<IInspectable>>
+    {
+        private readonly Lazy<IDataRepository<IInspectable>> _lazyInstance = new Lazy<IDataRepository<IInspectable>>(() => new DataRepository<IInspectable>());
+        
+        public IDataRepository<IInspectable> Provide() => _lazyInstance.Value;
+    }
+}

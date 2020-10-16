409a9e763291f78d1d9867f1b2ffdaa1b49f8c2b
noobnewbier7 <peter789b@gmail.com>
2020-10-16 21:11:16 +0800

    Added construct's inspector view

--- Assets/Code/Noneb/Ui/InGameEditor/Inspector/ConstructInspector/ConstructInspectorView.cs
+++ Assets/Code/Noneb/Ui/InGameEditor/Inspector/ConstructInspector/ConstructInspectorView.cs
@@ -1,9 +1,50 @@
-﻿using UnityUtils.Pooling;
+﻿using System;
+using Noneb.Core.Game.Constructs;
+using Noneb.Core.InGameEditor.Data;
+using TMPro;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace Noneb.Ui.InGameEditor.Inspector.ConstructInspector
 {
-    public class ConstructInspectorView : PooledMonoBehaviour
+    public class ConstructInspectorView : MonoBehaviour
     {
+        [SerializeField] private GameObject windowGameObject;
         
+        [SerializeField] private TextMeshProUGUI nameText;
+        [SerializeField] private Image iconImage;
+
+        [SerializeField] private ConstructInspectorViewModelFactory viewModelFactory;
+
+        private InspectorViewModel<PaletteData<Preset<ConstructData>>> _viewModel;
+        private IDisposable _disposable;
+
+        private void OnEnable()
+        {
+            _viewModel = viewModelFactory.Create();
+            _disposable = new CompositeDisposable
+            {
+                _viewModel.InspectableLiveData.Subscribe(OnUpdatePreset),
+                _viewModel.VisibilityLiveData.Subscribe(OnUpdateVisibility)
+            };
+        }
+
+        private void OnDisable()
+        {
+            _disposable.Dispose();
+            _viewModel.Dispose();
+        }
+
+        private void OnUpdatePreset(PaletteData<Preset<ConstructData>> preset)
+        {
+            nameText.text = $"Name: {preset.Name}";
+            iconImage.sprite = preset.Icon;
+        }
+
+        private void OnUpdateVisibility(bool isVisible)
+        {
+            windowGameObject.SetActive(isVisible);
+        }        
     }
 }
---
+++ Assets/Code/Noneb/Ui/InGameEditor/Inspector/ConstructInspector/ConstructInspectorViewModelFactory.cs
@@ -0,0 +1,22 @@
+﻿using Noneb.Core.Game.Common.Constants;
+using Noneb.Core.Game.Common.Factories;
+using Noneb.Core.Game.Constructs;
+using Noneb.Core.InGameEditor.Data;
+using Noneb.Ui.InGameEditor.UiState.Inspectable;
+using UnityEngine;
+using UnityUtils.Constants;
+
+namespace Noneb.Ui.InGameEditor.Inspector.ConstructInspector
+{
+    [CreateAssetMenu(
+        fileName = nameof(ConstructInspectorViewModelFactory),
+        menuName = MenuName.Factory + ProjectMenuName.InGameEditor + "ConstructInspectorViewModel"
+    )]
+    public class ConstructInspectorViewModelFactory : ScriptableObject, IFactory<InspectorViewModel<PaletteData<Preset<ConstructData>>>>
+    {
+        [SerializeField] private CurrentInspectableRepositoryProvider currentInspectableRepositoryProvider;
+
+        public InspectorViewModel<PaletteData<Preset<ConstructData>>> Create() =>
+            new InspectorViewModel<PaletteData<Preset<ConstructData>>>(currentInspectableRepositoryProvider.Provide());
+    }
+}

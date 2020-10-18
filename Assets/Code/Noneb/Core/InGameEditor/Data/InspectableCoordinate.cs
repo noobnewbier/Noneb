5fed1235336e652be4e925685e90552a4ef00ce8
noobnewbier7 <peter789b@gmail.com>
2020-10-18 17:39:49 +0800

    added concept of inspectable

---
+++ Assets/Code/Noneb/Core/InGameEditor/Data/InspectableCoordinate.cs
@@ -0,0 +1,15 @@
+﻿using Noneb.Core.Game.Coordinates;
+using Noneb.Core.InGameEditor.Common;
+
+namespace Noneb.Core.InGameEditor.Data
+{
+    public class InspectableCoordinate : IInspectable
+    {
+        public InspectableCoordinate(Coordinate coordinate)
+        {
+            Coordinate = coordinate;
+        }
+
+        public Coordinate Coordinate { get; }
+    }
+}

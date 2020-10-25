df0377c32c28ceb4d18af9e0cc80b8c686e959c6
noobnewbier7 <peter789b@gmail.com>
2020-10-26 02:52:33 +0800

    moving directory and naming refactor

---
+++ Assets/Code/Noneb/Core/Game/GameState/GameEnvironments/GameEnvironmentRepository.cs
@@ -0,0 +1,22 @@
+﻿using Noneb.Core.Game.Common;
+using Noneb.Core.Game.GameEnvironments.Data;
+
+namespace Noneb.Core.Game.GameState.CurrentGameEnvironments
+{
+    public interface IGameEnvironmentRepository : IGameEnvironmentGetRepository, IGameEnvironmentSetRepository
+    {
+    }
+
+    public interface IGameEnvironmentGetRepository : IDataGetRepository<GameEnvironment>
+    {
+    }
+
+    public interface IGameEnvironmentSetRepository : IDataSetRepository<GameEnvironment>
+    {
+    }
+
+    public class GameEnvironmentRepository : DataRepository<GameEnvironment>,
+                                                    IGameEnvironmentRepository
+    {
+    }
+}
---
+++ Assets/Code/Noneb/Core/Game/GameState/LevelDatas/SelectedLevelDataRepositoryProvider.cs
@@ -0,0 +1,25 @@
+﻿using Noneb.Core.Game.Common.Providers;
+using Noneb.Core.Game.GameState.CurrentGameEnvironments;
+using UnityEngine;
+using UnityEngine.Serialization;
+using UnityUtils.Constants;
+
+namespace Noneb.Core.Game.GameState.CurrentLevelDatas
+{
+    [CreateAssetMenu(
+        fileName = nameof(SelectedLevelDataRepositoryProvider),
+        menuName = MenuName.ScriptableRepository + nameof(LevelDataRepository)
+    )]
+    public class SelectedLevelDataRepositoryProvider : ScriptableObject, IObjectProvider<ILevelDataRepository>
+    {
+        [FormerlySerializedAs("currentGameEnvironmentRepositoryProvider")] [SerializeField] private SelectedGameEnvironmentRepositoryProvider selectedGameEnvironmentRepositoryProvider;
+
+        private ILevelDataRepository _cache;
+
+        public ILevelDataRepository Provide() =>
+            _cache ??
+            (_cache = new LevelDataRepository(
+                selectedGameEnvironmentRepositoryProvider.Provide()
+            ));
+    }
+}
